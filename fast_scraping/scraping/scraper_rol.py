b9d1a16cd72035ef2e01433bffe23e5813ac9a50
izabella-m <izabella.pessoa03@outlook.com>
2025-03-29 01:59:59 -0300

    feat: add status code check in download request

--- fast_scraping/scraping/scraper_rol.py
+++ fast_scraping/scraping/scraper_rol.py
@@ -3,8 +3,12 @@ import requests
 
 def download_rol(url, endereco):
     response = requests.get(url)
-    with open(endereco, 'wb') as novo_arquivo:
-        novo_arquivo.write(response.content)
+    if response.status_code == requests.codes.ok:
+        with open(endereco, 'wb') as novo_arquivo:
+            novo_arquivo.write(response.content)
+        print("Download concluído com sucesso!")
+    else:
+        response.raise_for_status()
 
 if __name__ == "__main__":
     download_rol("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf", 'testeanexo1.pdf')

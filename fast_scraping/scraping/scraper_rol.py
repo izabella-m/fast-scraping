3a602245d8a1440e57c5dac295a050b705fcb7c8
izabella-m <izabella.pessoa03@outlook.com>
2025-04-04 23:41:00 -0300

    refactor: renomeia o nome do arquivo salvo em pdf

--- fast_scraping/scraping/scraper_rol.py
+++ fast_scraping/scraping/scraper_rol.py
@@ -1,5 +1,5 @@
 import requests
-
+import data
 
 def download_rol(url, endereco):
     response = requests.get(url)
@@ -11,6 +11,5 @@ def download_rol(url, endereco):
         response.raise_for_status()
 
 if __name__ == "__main__":
-    download_rol("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf", 'testeanexo1.pdf')
-    download_rol("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf", 'testeanexo2.pdf')
-
+    download_rol("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf", '/Users/izabellamaria/fast-scraping/data/attachments/anexo1.pdf')
+    download_rol("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf", '/Users/izabellamaria/fast-scraping/data/attachments/anexo2.pdf')
